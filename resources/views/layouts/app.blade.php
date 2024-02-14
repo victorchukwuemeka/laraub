fc41b1adc2aa4b4e36b89c13131b9db44de58adb
victorchukwuemeka <chukwuemekavictor693@gmail.com>
2024-02-14 10:22:54 +0100

    npm edit

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <link rel="stylesheet" href="{{ asset('/css/trix.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 
     @vite('resources/css/app.css')
 
