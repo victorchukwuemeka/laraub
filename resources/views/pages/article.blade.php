c97bd726a089ad0f7e8ba180d91c04ae0000f9fa
viktr <chukwuemekavictor693@gmail.com>
2024-03-13 20:15:08 -0400

    ff

--- resources/views/pages/article.blade.php
+++ resources/views/pages/article.blade.php
@@ -15,7 +15,7 @@
 <!-- Featured Articles -->
 <section class="py-10 px-5  sm:py-10">
 
-    <div class="container mx-auto grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 md:grid-cols-3 gap-8">
+    <div class="container mx-auto grid grid-cols-3 sm:grid-cols-1 lg:grid-cols-3 md:grid-cols-3 gap-8">
      <!-- grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 gap-8-->
         @foreach($viewData['articles'] as $article)
         <div>
