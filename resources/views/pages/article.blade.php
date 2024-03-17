2bd0a9aa8d9445b65d35b23085f7384ece96a8de
viktr <chukwuemekavictor693@gmail.com>
2024-03-16 23:50:42 -0400

    hhg

--- resources/views/pages/article.blade.php
+++ resources/views/pages/article.blade.php
@@ -9,34 +9,29 @@
 </div>
 
 <!-- Featured Articles -->
-<div>
-    <div>
-        @foreach($viewData['articles'] as $article)
-        <div>
-            <img src="{{ asset('/img/silicon.png') }}" alt="Article 1">
-            <div>
-                <h2>{{ $article->get_title() }}</h2>
-                <p>
-                    @php
-                     $body = $article->get_body();
-                     $lent = strlen($body);
-                     if($lent > 200){
-                       $body = substr($body, 0, 200);
-                     }else{
-                       $body;
-                     }
-                    @endphp
-                    {!! $body !!}
-                </p>
-            </div>
-            <div>
-                <a href="{{ url('/show_article/'.$article->get_id()) }}">Read More</a>
-            </div>
+<div class="flex flex-wrap justify-center gap-8">
+    @foreach($viewData['articles'] as $article)
+    <div class="max-w-md rounded overflow-hidden shadow-lg bg-white flex">
+        <img class="w-1/3" src="{{ asset('/img/silicon.png') }}" alt="Article 1">
+        <div class="p-4 w-2/3">
+            <h2>{{ $article->get_title() }}</h2>
+            <p>
+                @php
+                 $body = $article->get_body();
+                 $lent = strlen($body);
+                 if($lent > 200){
+                   $body = substr($body, 0, 200);
+                 }else{
+                   $body;
+                 }
+                @endphp
+                {!! $body !!}
+            </p>
+            <a href="{{ url('/show_article/'.$article->get_id()) }}">Read More</a>
         </div>
-        @endforeach
     </div>
+    @endforeach
 </div>
-
 
 
 @endsection
--- storage/framework/views/c69dbec3767a517ea2d0a773f2511a20.php
+++ storage/framework/views/c69dbec3767a517ea2d0a773f2511a20.php
@@ -8,35 +8,30 @@
 </div>
 
 <!-- Featured Articles -->
-<div>
-    <div>
-        <?php $__currentLoopData = $viewData['articles']; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $article): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
-        <div>
-            <img src="<?php echo e(asset('/img/silicon.png')); ?>" alt="Article 1">
-            <div>
-                <h2><?php echo e($article->get_title()); ?></h2>
-                <p>
-                    <?php
-                     $body = $article->get_body();
-                     $lent = strlen($body);
-                     if($lent > 200){
-                       $body = substr($body, 0, 200);
-                     }else{
-                       $body;
-                     }
-                    ?>
-                    <?php echo $body; ?>
+<div class="flex flex-wrap justify-center gap-8">
+    <?php $__currentLoopData = $viewData['articles']; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $article): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+    <div class="max-w-md rounded overflow-hidden shadow-lg bg-white flex">
+        <img class="w-1/3" src="<?php echo e(asset('/img/silicon.png')); ?>" alt="Article 1">
+        <div class="p-4 w-2/3">
+            <h2><?php echo e($article->get_title()); ?></h2>
+            <p>
+                <?php
+                 $body = $article->get_body();
+                 $lent = strlen($body);
+                 if($lent > 200){
+                   $body = substr($body, 0, 200);
+                 }else{
+                   $body;
+                 }
+                ?>
+                <?php echo $body; ?>
 
-                </p>
-            </div>
-            <div>
-                <a href="<?php echo e(url('/show_article/'.$article->get_id())); ?>">Read More</a>
-            </div>
+            </p>
+            <a href="<?php echo e(url('/show_article/'.$article->get_id())); ?>">Read More</a>
         </div>
-        <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
     </div>
+    <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
 </div>
-
 
 
 <?php $__env->stopSection(); ?>
