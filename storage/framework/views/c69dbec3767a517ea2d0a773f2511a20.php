d8ab6f570edff39ffc1893236fba025cbb3b5d80
viktr <chukwuemekavictor693@gmail.com>
2024-03-16 23:48:13 -0400

    hhg

--- storage/framework/views/c69dbec3767a517ea2d0a773f2511a20.php
+++ storage/framework/views/c69dbec3767a517ea2d0a773f2511a20.php
@@ -1,25 +1,21 @@
 <?php $__env->startSection('content'); ?>
-<div class="bg-gray-900 text-white py-8 sm:py-16">
-    <div class="container mx-auto text-center">
-        <h1 class="text-3xl sm:text-5xl font-extrabold mb-4">Welcome To laraub</h1>
-        <p class="text-lg sm:text-xl mb-8">
-            Stay updated with the latest tech news and trends on Laravel.
-        </p>
-        <a href="<?php echo e(route("create.article")); ?>" class="bg-blue-500 hover:bg-blue-600 text-white py-2 px-6 sm:py-3 sm:px-8 rounded-full text-lg sm:text-xl transition duration-300 ease-in-out inline-block">
-           care to write
-        </a>
+<div>
+    <div>
+        <h1>Welcome To laraub</h1>
+        <p>Stay updated with the latest tech news and trends on Laravel.</p>
+        <a href="<?php echo e(route("create.article")); ?>">Care to write</a>
     </div>
 </div>
 
 <!-- Featured Articles -->
-<div class="py-10 px-5 sm:py-10">
-    <div class="container mx-auto flex flex-wrap justify-center gap-8">
+<div>
+    <div>
         <?php $__currentLoopData = $viewData['articles']; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $article): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
-        <div class="max-w-sm rounded overflow-hidden shadow-lg bg-white flex flex-col">
-            <img class="w-full" src="<?php echo e(asset('/img/silicon.png')); ?>" alt="Article 1">
-            <div class="px-6 py-4 flex-grow">
-                <div class="font-bold text-xl mb-2"><?php echo e($article->get_title()); ?></div>
-                <p class="text-gray-700 text-base flex-grow">
+        <div>
+            <img src="<?php echo e(asset('/img/silicon.png')); ?>" alt="Article 1">
+            <div>
+                <h2><?php echo e($article->get_title()); ?></h2>
+                <p>
                     <?php
                      $body = $article->get_body();
                      $lent = strlen($body);
@@ -33,8 +29,8 @@
 
                 </p>
             </div>
-            <div class="px-6 pt-4 pb-2">
-                <a href="<?php echo e(url('/show_article/'.$article->get_id())); ?>" class="text-blue-500 hover:underline text-sm">Read More</a>
+            <div>
+                <a href="<?php echo e(url('/show_article/'.$article->get_id())); ?>">Read More</a>
             </div>
         </div>
         <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
